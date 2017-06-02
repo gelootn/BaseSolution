78f6ff49746d3194304cb8807bc60714e2a17b2f
Angelo Dejaeghere <angelo.dejaeghere@gmail.com>
2017-06-02 13:01:33 +0200

    Basic generic repository

---
+++ BaselineSolution.DAL/Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using BaselineSolution.DAL.Infrastructure.Bases;
+
+namespace BaselineSolution.DAL.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : Entity
+    {
+        IQueryable<TEntity> GetAll();
+        TEntity Find(int id);
+        TEntity AddOrUpdate(TEntity iten);
+        void Delete(int id);
+
+
+    }
+}
