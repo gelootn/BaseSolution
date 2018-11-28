71cf0db59221a04b97c6905fe4823dd68dce9c25
Angelo Dejaeghere <angelo.dejaeghere@gmail.com>
2018-11-28 14:45:28 +0100

    NewtonSoft package upgrade + Rhinomock convert to Moq

--- BasaelineSolution.WebApi.Tests/AccountControllerTests.cs
+++ BasaelineSolution.WebApi.Tests/AccountControllerTests.cs
@@ -7,12 +7,12 @@ using BaselineSolution.WebApi.Controllers;
 using BaselineSolution.WebApi.Filters.Account;
 using BaselineSolution.WebApi.Infrastructure.Filters;
 using NUnit.Framework;
-using Rhino.Mocks;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Results;
 using BaselineSolution.Framework.Infrastructure.Filtering;
+using Moq;
 
 namespace BasaelineSolution.WebApi.Tests
 {
@@ -20,18 +20,20 @@ namespace BasaelineSolution.WebApi.Tests
     public class AccountControllerTests
     {
         private AccountController _controller;
-        private IGenericService<AccountBo> _accountService;
-        private IFilterHandler<AccountBo, AccountBoFilter> _listFilterHandler;
-        private ILogging _logger;
+        private Mock<IGenericService<AccountBo>> _accountService;
+        private Mock<IFilterHandler<AccountBo, AccountBoFilter>> _listFilterHandler;
+        private Mock<ILogging> _logger;
 
         [SetUp]
         public void SetUp()
         {
-            _accountService = MockRepository.GenerateMock<IGenericService<AccountBo>>();
-            _listFilterHandler = MockRepository.GenerateMock<IFilterHandler<AccountBo, AccountBoFilter>>();
-            _logger = MockRepository.GenerateMock<ILogging>();
 
-            _controller = new AccountController(_accountService, _listFilterHandler, _logger);
+
+            _accountService = new Mock<IGenericService<AccountBo>>(); 
+            _listFilterHandler = new Mock<IFilterHandler<AccountBo, AccountBoFilter>>();
+            _logger = new Mock<ILogging>();
+
+            _controller = new AccountController(_accountService.Object, _listFilterHandler.Object, _logger.Object);
             _controller.Request = new HttpRequestMessage();
             _controller.Configuration = new HttpConfiguration();
         }
@@ -42,8 +44,8 @@ namespace BasaelineSolution.WebApi.Tests
             var accountBo = new AccountBo { Id = 1, Name = "Test Account" };
             var response = new Response<AccountBo>(accountBo);
 
-            _accountService.Expect(x => x.GetByIdAsync(Arg<int>.Is.Anything))
-                .Return(Task.FromResult(response));
+            _accountService.Setup(x => x.GetByIdAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(response));
 
             var actionResult = _controller.GetById(1);
             var contentResult = actionResult.Result as OkNegotiatedContentResult<ApiResponse<AccountBo>>;
@@ -65,8 +67,9 @@ namespace BasaelineSolution.WebApi.Tests
 
             var response = new Response<AccountBo>(new List<AccountBo>{accountOne, accountTwo});
 
-            _accountService.Expect(x => x.ListAsync(null))
-                .Return(Task.FromResult(response));
+ 
+            _accountService.Setup(x => x.ListAsync(null))
+                .Returns(Task.FromResult(response));
 
             var actionResult = _controller.List(null);
             var contentResult = actionResult.Result as OkNegotiatedContentResult<ApiResponse<AccountBo>>;
@@ -89,17 +92,17 @@ namespace BasaelineSolution.WebApi.Tests
             var filter = new AccountBoFilter{ Name = "Test" };
             var repoFilter = EntityFilter<AccountBo>.Where(x => x.Name.Contains(filter.Name));
 
-            _listFilterHandler.Expect(x => x.CreateFilter(filter))
-                .Return(repoFilter);
+            _listFilterHandler.Setup(x => x.CreateFilter(filter))
+                .Returns(repoFilter);
 
-            _accountService.Expect(x => x.ListAsync(repoFilter))
-                .Return(Task.FromResult(serviceResponse));
+            _accountService.Setup(x => x.ListAsync(repoFilter))
+                .Returns(Task.FromResult(serviceResponse));
 
             var actionResult = _controller.List(filter);
             var contentResult = actionResult.Result as OkNegotiatedContentResult<ApiResponse<AccountBo>>;
 
-            _listFilterHandler.VerifyAllExpectations();
-            _accountService.VerifyAllExpectations();
+            _listFilterHandler.VerifyAll();
+            _accountService.VerifyAll();
 
             Assert.That(contentResult, Is.Not.Null);
             Assert.That(contentResult.Content, Is.Not.Null);
